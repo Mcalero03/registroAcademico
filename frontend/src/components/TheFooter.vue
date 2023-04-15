62d8d4c86d2ee3abe6ec433ce660e0d730a5d136
Leonel Lopez <leonellopez647@gmail.com>
2023-04-15 06:45:38 -0600

    first commit

---
+++ frontend/src/components/TheFooter.vue
@@ -0,0 +1,34 @@
+<template>
+  <footer class="mt-5 mb-3">
+    <v-container>
+      <v-row class="text-center">
+        <v-col cols="12" sm="12" md="4" class="my-auto">
+          <a href="https://cultura.gob.sv" target="__blank"
+            ><b>Ministerio de Cultura</b></a
+          >
+        </v-col>
+        <v-col cols="12" sm="12" md="4">
+          <v-img
+            src="/images/logo_goes.svg"
+            alt=""
+            class="mx-auto text-center"
+            style="max-width: 350px; height: auto"
+          />
+        </v-col>
+        <v-col cols="12" sm="12" md="4" class="my-auto">
+          <b>Bases de datos de desarrollo</b>
+        </v-col>
+      </v-row>
+    </v-container>
+  </footer>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+a {
+  text-decoration: none;
+  color: black;
+}
+</style>
---
+++ frontend/src/main.js
@@ -0,0 +1,33 @@
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import { createVuetify } from "vuetify";
+
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import { es } from "vuetify/locale";
+
+import App from "./App.vue";
+import router from "./router";
+
+// Importing styles
+import "./assets/styles/main.scss";
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  locale: {
+    locale: "es",
+    messages: { es },
+  },
+  icons: {
+    defaultSet: "mdi", // This is already the default value - only for display purposes
+  },
+});
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(vuetify);
+app.use(router);
+
+app.mount("#app");
