6f2b7fc8f7d45596286060b7909d34f1d9dc63a4
Mcalero03 <mcalero@cultura.gob.sv>
2023-07-24 15:27:17 -0600

    Triggers and events on migration

--- backend/database/migrations/2023_07_20_131527_add_trigger.php
+++ backend/database/migrations/2023_07_20_131527_add_trigger.php
@@ -12,7 +12,7 @@ class AddTrigger extends Migration
         // Desactivamos el chequeo de claves foráneas para poder crear los triggers
         Schema::disableForeignKeyConstraints();
 
-        // // Trigger update_CycleInscriptionStatus
+        // Trigger update_CycleInscriptionStatus
         // DB::unprepared('
         //     CREATE TRIGGER update_CycleInscriptionStatus
         //     AFTER UPDATE ON inscription_detail
@@ -20,45 +20,26 @@ class AddTrigger extends Migration
         //     BEGIN
         //         DECLARE total_retirados INT;
         //         DECLARE total_inscription_details INT;
-        //         IF (NEW.status = "Retirado") THEN
-        //             -- Obtener el total de inscription_detail con estado "Retirado"
-        //             SELECT COUNT(*) INTO total_retirados
-        //             FROM inscription_detail
-        //             WHERE inscription_id = NEW.inscription_id AND status = "Retirado";
-        //             -- Obtener el total de inscription_detail asociados a la misma inscription
-        //             SELECT COUNT(*) INTO total_inscription_details
-        //             FROM inscription_detail
-        //             WHERE inscription_id = NEW.inscription_id;
 
+        //         -- Obtener el total de inscription_detail con estado "Retirado"
+        //         SELECT COUNT(*) INTO total_retirados
+        //         FROM inscription_detail
+        //         WHERE inscription_id = NEW.inscription_id AND status = "Retirado";
+
+        //         -- Obtener el total de inscription_detail asociados a la misma inscription
+        //         SELECT COUNT(*) INTO total_inscription_details
+        //         FROM inscription_detail
+        //         WHERE inscription_id = NEW.inscription_id;
+
+        //         IF (NEW.status = "Retirado") THEN
         //             -- Si todos los inscription_detail están "Retirado", cambiar estado de inscription
         //             IF total_retirados = total_inscription_details THEN
         //                 UPDATE inscription
         //                 SET status = "Retirado"
         //                 WHERE id = NEW.inscription_id;
         //             END IF;
-        //         END IF;
-        //     END
-        // ');
-
-        // // Trigger update_CycleInscriptionStatusActive
-        // DB::unprepared('
-        //     CREATE TRIGGER update_CycleInscriptionStatusActive
-        //     AFTER UPDATE ON inscription_detail
-        //     FOR EACH ROW
-        //     BEGIN
-        //         DECLARE total_retirados INT;
-        //         DECLARE total_inscription_details INT;
-        //         IF (NEW.status = "Inscrito") THEN
-        //             -- Obtener el total de inscription_detail con estado "Retirado"
-        //             SELECT COUNT(*) INTO total_retirados
-        //             FROM inscription_detail
-        //             WHERE inscription_id = NEW.inscription_id AND status = "Retirado";
-        //             -- Obtener el total de inscription_detail asociados a la misma inscription
-        //             SELECT COUNT(*) INTO total_inscription_details
-        //             FROM inscription_detail
-        //             WHERE inscription_id = NEW.inscription_id;
-
-        //             -- Si todos los inscription_detail están "Retirado", cambiar estado de inscription
+        //         ELSEIF (NEW.status = "Inscrito") THEN
+        //             -- Si no todos los inscription_detail están "Retirado", cambiar estado de inscription
         //             IF total_retirados != total_inscription_details THEN
         //                 UPDATE inscription
         //                 SET status = "Inscrito"
@@ -70,40 +51,34 @@ class AddTrigger extends Migration
 
         // Trigger update_ApprovalInscriptionStatus
         DB::unprepared('
-            CREATE TRIGGER update_ApprovalInscriptionStatus
+            CREATE TRIGGER update_ApprovalInscriptionStatus 
             AFTER UPDATE ON calification
             FOR EACH ROW
-            BEGIN
+            BEGIN 
                 DECLARE total_average DECIMAL(10,2);
                 DECLARE average_approval DECIMAL(10,2);
 
-                SELECT ROUND(SUM((calification.score*evaluation.ponder)/100), 2) INTO total_average
-                FROM inscription
-                JOIN inscription_detail AS i ON inscription.id = i.inscription_id
-                JOIN calification ON i.id = calification.inscription_detail_id
-                JOIN evaluation ON calification.evaluation_id = evaluation.id
-                JOIN `group` ON i.group_id = `group`.id
-                JOIN subject ON `group`.subject_id = subject.id
-                JOIN student ON inscription.student_id = student.id
-                JOIN pensum_subject_detail ON subject.id = pensum_subject_detail.subject_id
-                JOIN pensum ON pensum_subject_detail.pensum_id = pensum.id
-                JOIN cycle ON inscription.cycle_id = cycle.id
-                WHERE calification.inscription_detail_id = NEW.inscription_detail_id
-                AND calification.deleted_at IS NULL
-                AND i.status NOT LIKE "Retirado"
-                AND cycle.status = "Activo"
-                GROUP BY subject.subject_name;
+            SELECT ROUND(SUM((calification.score*evaluation.ponder)/100), 2) into total_average
+            FROM inscription
+            JOIN inscription_detail AS i ON inscription.id = i.inscription_id
+            JOIN calification ON i.id = calification.inscription_detail_id
+            JOIN evaluation ON calification.evaluation_id = evaluation.id
+            JOIN cycle ON inscription.cycle_id = cycle.id
+            WHERE calification.inscription_detail_id = NEW.inscription_detail_id
+            AND calification.deleted_at IS NULL
+            AND i.status NOT LIKE "Retirado"
+            AND cycle.status = "Activo";  
 
-                SELECT subject.average_approval INTO average_approval
-                FROM inscription
-                JOIN inscription_detail AS i ON inscription.id = i.inscription_id
-                JOIN `group` ON i.group_id = `group`.id
-                JOIN subject ON `group`.subject_id = subject.id
-                JOIN cycle ON inscription.cycle_id = cycle.id
-                WHERE i.id = NEW.inscription_detail_id
-                AND i.deleted_at IS NULL;
+            SELECT subject.average_approval into average_approval
+            FROM inscription
+            JOIN inscription_detail AS i ON inscription.id = i.inscription_id
+            JOIN `group` ON i.group_id = `group`.id
+            JOIN subject ON `group`.subject_id = subject.id
+            JOIN cycle ON inscription.cycle_id = cycle.id
+            WHERE i.id = NEW.inscription_detail_id
+            AND i.deleted_at IS NULL;
 
-                IF (total_average >= average_approval) THEN
+            IF (total_average >= average_approval) THEN
                     UPDATE inscription_detail 
                     SET status = "Aprobado"
                     WHERE id = NEW.inscription_detail_id;
@@ -117,40 +92,34 @@ class AddTrigger extends Migration
 
         // Trigger insert_ApprovalInscriptionStatus
         DB::unprepared('
-            CREATE TRIGGER insert_ApprovalInscriptionStatus
+            CREATE TRIGGER insert_ApprovalInscriptionStatus 
             AFTER INSERT ON calification
             FOR EACH ROW
-            BEGIN
+            BEGIN 
                 DECLARE total_average DECIMAL(10,2);
                 DECLARE average_approval DECIMAL(10,2);
 
-                SELECT ROUND(SUM((calification.score*evaluation.ponder)/100), 2) INTO total_average
-                FROM inscription
-                JOIN inscription_detail AS i ON inscription.id = i.inscription_id
-                JOIN calification ON i.id = calification.inscription_detail_id
-                JOIN evaluation ON calification.evaluation_id = evaluation.id
-                JOIN `group` ON i.group_id = `group`.id
-                JOIN subject ON `group`.subject_id = subject.id
-                JOIN student ON inscription.student_id = student.id
-                JOIN pensum_subject_detail ON subject.id = pensum_subject_detail.subject_id
-                JOIN pensum ON pensum_subject_detail.pensum_id = pensum.id
-                JOIN cycle ON inscription.cycle_id = cycle.id
-                WHERE calification.inscription_detail_id = NEW.inscription_detail_id
-                AND calification.deleted_at IS NULL
-                AND i.status NOT LIKE "Retirado"
-                AND cycle.status = "Activo"
-                GROUP BY subject.subject_name;
+            SELECT ROUND(SUM((calification.score*evaluation.ponder)/100), 2) into total_average
+            FROM inscription
+            JOIN inscription_detail AS i ON inscription.id = i.inscription_id
+            JOIN calification ON i.id = calification.inscription_detail_id
+            JOIN evaluation ON calification.evaluation_id = evaluation.id
+            JOIN cycle ON inscription.cycle_id = cycle.id
+            WHERE calification.inscription_detail_id = NEW.inscription_detail_id
+            AND calification.deleted_at IS NULL
+            AND i.status NOT LIKE "Retirado"
+            AND cycle.status = "Activo";  
 
-                SELECT subject.average_approval INTO average_approval
-                FROM inscription
-                JOIN inscription_detail AS i ON inscription.id = i.inscription_id
-                JOIN `group` ON i.group_id = `group`.id
-                JOIN subject ON `group`.subject_id = subject.id
-                JOIN cycle ON inscription.cycle_id = cycle.id
-                WHERE i.id = NEW.inscription_detail_id
-                AND i.deleted_at IS NULL;
+            SELECT subject.average_approval into average_approval
+            FROM inscription
+            JOIN inscription_detail AS i ON inscription.id = i.inscription_id
+            JOIN `group` ON i.group_id = `group`.id
+            JOIN subject ON `group`.subject_id = subject.id
+            JOIN cycle ON inscription.cycle_id = cycle.id
+            WHERE i.id = NEW.inscription_detail_id
+            AND i.deleted_at IS NULL;
 
-                IF (total_average >= average_approval) THEN
+            IF (total_average >= average_approval) THEN
                     UPDATE inscription_detail 
                     SET status = "Aprobado"
                     WHERE id = NEW.inscription_detail_id;
@@ -159,45 +128,39 @@ class AddTrigger extends Migration
                     SET status = "Reprobado"
                     WHERE id = NEW.inscription_detail_id;
                 END IF;
-            END
+            END 
         ');
 
         // Trigger delete_ApprovalInscriptionStatus
         DB::unprepared('
-            CREATE TRIGGER delete_ApprovalInscriptionStatus
+            CREATE TRIGGER delete_ApprovalInscriptionStatus 
             AFTER DELETE ON calification
             FOR EACH ROW
-            BEGIN
+            BEGIN 
                 DECLARE total_average DECIMAL(10,2);
                 DECLARE average_approval DECIMAL(10,2);
 
-                SELECT ROUND(SUM((calification.score*evaluation.ponder)/100), 2) INTO total_average
-                FROM inscription
-                JOIN inscription_detail AS i ON inscription.id = i.inscription_id
-                JOIN calification ON i.id = calification.inscription_detail_id
-                JOIN evaluation ON calification.evaluation_id = evaluation.id
-                JOIN `group` ON i.group_id = `group`.id
-                JOIN subject ON `group`.subject_id = subject.id
-                JOIN student ON inscription.student_id = student.id
-                JOIN pensum_subject_detail ON subject.id = pensum_subject_detail.subject_id
-                JOIN pensum ON pensum_subject_detail.pensum_id = pensum.id
-                JOIN cycle ON inscription.cycle_id = cycle.id
-                WHERE calification.inscription_detail_id = OLD.inscription_detail_id
-                AND calification.deleted_at IS NULL
-                AND i.status NOT LIKE "Retirado"
-                AND cycle.status = "Activo"
-                GROUP BY subject.subject_name;
+            SELECT ROUND(SUM((calification.score*evaluation.ponder)/100), 2) into total_average
+            FROM inscription
+            JOIN inscription_detail AS i ON inscription.id = i.inscription_id
+            JOIN calification ON i.id = calification.inscription_detail_id
+            JOIN evaluation ON calification.evaluation_id = evaluation.id
+            JOIN cycle ON inscription.cycle_id = cycle.id
+            WHERE calification.inscription_detail_id = OLD.inscription_detail_id
+            AND calification.deleted_at IS NULL
+            AND i.status NOT LIKE "Retirado"
+            AND cycle.status = "Activo";  
 
-                SELECT subject.average_approval INTO average_approval
-                FROM inscription
-                JOIN inscription_detail AS i ON inscription.id = i.inscription_id
-                JOIN `group` ON i.group_id = `group`.id
-                JOIN subject ON `group`.subject_id = subject.id
-                JOIN cycle ON inscription.cycle_id = cycle.id
-                WHERE i.id = OLD.inscription_detail_id
-                AND i.deleted_at IS NULL; 
+            SELECT subject.average_approval into average_approval
+            FROM inscription
+            JOIN inscription_detail AS i ON inscription.id = i.inscription_id
+            JOIN `group` ON i.group_id = `group`.id
+            JOIN subject ON `group`.subject_id = subject.id
+            JOIN cycle ON inscription.cycle_id = cycle.id
+            WHERE i.id = OLD.inscription_detail_id
+            AND i.deleted_at IS NULL;
 
-                IF (total_average >= average_approval) THEN
+            IF (total_average >= average_approval) THEN
                     UPDATE inscription_detail 
                     SET status = "Aprobado"
                     WHERE id = OLD.inscription_detail_id;
@@ -215,8 +178,7 @@ class AddTrigger extends Migration
 
     public function down()
     {
-        // DB::unprepared('DROP TRIGGER `update_CycleInscriptionStatus`');
-        // DB::unprepared('DROP TRIGGER `update_CycleInscriptionStatusActive`');
+        DB::unprepared('DROP TRIGGER `update_CycleInscriptionStatus`');
         DB::unprepared('DROP TRIGGER `update_ApprovalInscriptionStatus`');
         DB::unprepared('DROP TRIGGER `insert_ApprovalInscriptionStatus`');
         DB::unprepared('DROP TRIGGER `delete_ApprovalInscriptionStatus`');
